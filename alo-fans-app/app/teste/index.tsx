d5bd8a937ecfb6c1060d3546ccf6c5bbdc36697e
Junior Prado <juniorcvnnn@gmail.com.br>
2024-07-04 13:58:37 -0300

    add teterste rou

---
+++ alo-fans-app/app/teste/index.tsx
@@ -0,0 +1,25 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { Link } from 'expo-router'
+
+const Teste = () => {
+  return (
+    <SafeAreaView style={{
+        display:"flex",
+        backgroundColor: "salmon",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100%"
+       ,padding: 20
+    }}>
+        <Text>Teste</Text>  
+        <Link href={"../teste/comHead"}>Com Head</Link>
+        <Link href={"../teste/semHead"}>Sem Head</Link>
+    </SafeAreaView>
+  )
+}
+
+export default Teste
+
+const styles = StyleSheet.create({})
