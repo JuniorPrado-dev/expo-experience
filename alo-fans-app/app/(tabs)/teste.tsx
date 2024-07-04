2480dcf9d8f56c285cf3e9a0be5d6065e9eee1d7
JuniorPrado-dev <juniorcvnnn@gmail.com>
2024-07-04 01:05:56 -0300

    CUSTON CHECKBOX

--- alo-fans-app/app/(tabs)/teste.tsx
+++ alo-fans-app/app/(tabs)/teste.tsx
@@ -4,6 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   const [isChecked, setChecked] = useState(false);
+  const [isChecked1, setChecked1] = useState(false);
+  const [isChecked2, setChecked2] = useState(false);
 
   return (
     <View style={styles.container}>
@@ -14,14 +16,17 @@ export default function App() {
       <View style={styles.section}>
         <Checkbox
           style={styles.checkbox}
-          value={isChecked}
-          onValueChange={setChecked}
+          value={isChecked1}
+          onValueChange={setChecked1}
           color={isChecked ? '#4630EB' : undefined}
         />
         <Text style={styles.paragraph}>Custom colored checkbox</Text>
       </View>
       <View style={styles.section}>
-        <Checkbox style={styles.checkbox} disabled value={isChecked} onValueChange={setChecked} />
+        <Checkbox 
+            style={styles.checkbox} 
+            disabled={!(isChecked && isChecked1)} 
+            value={isChecked2} onValueChange={setChecked2} />
         <Text style={styles.paragraph}>Disabled checkbox</Text>
       </View>
     </View>
