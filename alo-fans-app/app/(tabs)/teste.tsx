47d6f2bfee2e5c19ee8a9c57cc803e749385010b
JuniorPrado-dev <juniorcvnnn@gmail.com>
2024-07-08 00:57:10 -0300

    add colors adnd tema

--- alo-fans-app/app/(tabs)/teste.tsx
+++ alo-fans-app/app/(tabs)/teste.tsx
@@ -1,52 +1,66 @@
-import Checkbox from 'expo-checkbox';
-import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Checkbox from "expo-checkbox";
+import React, { useState } from "react";
+import { Button, Pressable, StyleSheet, Text, View } from "react-native";
+import { TouchableHighlight } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
   const [isChecked, setChecked] = useState(false);
   const [isChecked1, setChecked1] = useState(false);
   const [isChecked2, setChecked2] = useState(false);
+  const [darkMode, setDarkMode] = useState(false);
+  console.log(darkMode);
 
   return (
-    <View style={styles.container}>
-      <View style={styles.section}>
-        <Checkbox style={styles.checkbox} value={isChecked} onValueChange={setChecked} />
-        <Text style={styles.paragraph}>Normal checkbox</Text>
+    <SafeAreaView
+      className={`w-full h-full mx-1 my-1 flex-1 ${darkMode ? "bg-secondaryDark" : "bg-secondary"} center`}
+    >
+      <View className={`h-full w-full ${darkMode ? "bg-secondaryDark" : "bg-secondary"}`}>
+        <View className="flex-row center">
+          <Checkbox
+            className={`m-1 border-1 ${darkMode ? "border-secondaryDark" : "bg-secondary"}`}
+            value={isChecked}
+            onValueChange={setChecked}
+          />
+          <Text className={`text-base ${darkMode ? "text-primaryDark" : "text-primary"}`}>
+            Normal checkbox
+          </Text>
+        </View>
+        <View className="flex-row center">
+          <Checkbox
+            className={`m-1 border-1 ${darkMode ? "border-secondaryDark" : "bg-secondary"}`}
+            value={isChecked1}
+            onValueChange={setChecked1}
+            color={isChecked ? "#4630EB" : undefined}
+          />
+          <Text className={`text-base ${darkMode ? "text-primaryDark" : "text-primary"}`}>
+            Custom colored checkbox
+          </Text>
+        </View>
+        <View className="flex-row center">
+          <Checkbox
+            className={`m-1 border-1 ${darkMode ? "border-secondaryDark" : "bg-secondary"}`}
+            disabled={!(isChecked && isChecked1)}
+            value={isChecked2}
+            onValueChange={setChecked2}
+          />
+          <Text className={`text-base ${darkMode ? "text-primaryDark" : "text-primary"}`}>
+            Disabled checkbox
+          </Text>
+        </View>
+        <View>
+          <TouchableHighlight
+            onPress={() => setDarkMode(!darkMode)}
+            className={` p-2 w-full border-2 border-primary${
+              darkMode ? "Dark" : " "
+            } p bg-secondary${darkMode ? "Dark" : " "} items-center`}
+          >
+            <Text className={`${darkMode ? "text-primaryDark" : "text-primary"}`}>
+              {darkMode?"Tema claro":"Tema Escuro"}
+            </Text>
+          </TouchableHighlight>
+        </View>
       </View>
-      <View style={styles.section}>
-        <Checkbox
-          style={styles.checkbox}
-          value={isChecked1}
-          onValueChange={setChecked1}
-          color={isChecked ? '#4630EB' : undefined}
-        />
-        <Text style={styles.paragraph}>Custom colored checkbox</Text>
-      </View>
-      <View style={styles.section}>
-        <Checkbox 
-            style={styles.checkbox} 
-            disabled={!(isChecked && isChecked1)} 
-            value={isChecked2} onValueChange={setChecked2} />
-        <Text style={styles.paragraph}>Disabled checkbox</Text>
-      </View>
-    </View>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginHorizontal: 16,
-    marginVertical: 32,
-  },
-  section: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  paragraph: {
-    fontSize: 15,
-  },
-  checkbox: {
-    margin: 8,
-  },
-});
--- alo-fans-app/tailwind.config.js
+++ alo-fans-app/tailwind.config.js
@@ -3,8 +3,16 @@ module.exports = {
   // NOTE: Update this to include the paths to all of your component files.
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
+  darkTheme:"class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#1F2937",
+        secondary: "#F7FAFC",
+        secondaryDark: "#1F2937",
+        primaryDark: "#F7FAFC",
+      }
+    },
   },
   plugins: [],
 }
